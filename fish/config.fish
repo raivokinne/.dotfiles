5d217c530419fcaf3b1a5b520b101b8b419534d1
RaivoKinne <raivokinne236@gmail.com>
2024-01-14 12:58:47 +0200

    Intial Commit

---
+++ fish/config.fish
@@ -0,0 +1,31 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias vim="nvim"
+alias ls="exa"
+
+# Add Homebrew to PATH
+set -gx PATH /usr/local/Homebrew/bin $PATH
+
+# Add Homebrew binaries to fish_user_paths
+set -gx fish_user_paths /usr/local/Homebrew/bin $fish_user_paths
+
+fish_add_path /opt/homebrew/bin
+set -g fish_greeting
+
+set --export GOPATH "$HOME/go"
+set --export PATH $GOPATH/bin $PATH
+
+set --export GOBIN "$GOPATH/bin"
+set --export PATH $GOBIN $PATH
+
+set --export COMPOSER_HOME "$HOME/.composer"
+set --export PATH $COMPOSER_HOME/vendor/bin $PATH
+
+set --export CARGO_HOME "$HOME/.cargo"
+set --export PATH $CARGO_HOME/bin $PATH
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
